0db0ebc35fdf5a69eb7c26e605c196079a9d1b48
hexiy <16806036+hexiy@users.noreply.github.com>
2023-04-23 22:20:58 +0200

    .

--- Tofu3D/Editor/RiderIDE.cs
+++ Tofu3D/Editor/RiderIDE.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 
 namespace Tofu3D;
@@ -6,6 +7,8 @@ namespace Tofu3D;
 // ReSharper disable once InconsistentNaming
 public static class RiderIDE
 {
+	static string _cachedRiderPath = "";
+
 	public static void OpenStackTrace(StackFrame stackFrame)
 	{
 		if (stackFrame.FileFullPath?.Length == 0)
@@ -15,12 +18,25 @@ public static class RiderIDE
 
 		try
 		{
-			var x = Environment.GetEnvironmentVariable("Path", EnvironmentVariableTarget.User);
-			string[] paths = x.Split(";");
-			string riderPath = paths.First((s => s.ToLower().Contains("rider")));
+			if (_cachedRiderPath.Length == 0)
+			{
+				var x = Environment.GetEnvironmentVariable("Path", EnvironmentVariableTarget.User);
+				string[] paths = x.Split(";");
+				string riderPath = paths.FirstOrDefault((s => s.ToLower().Contains("jetbrains rider")), "");
+
+				if (riderPath.Length == 0 || Directory.Exists(riderPath) == false)
+				{
+					Debug.LogError("Couldn't find Rider in envornment variable 'Path'");
+					return;
+				}
+
+				_cachedRiderPath = riderPath;
+			}
+
+
 			Process.Start(startInfo: new ProcessStartInfo()
 			                         {
-				                         WorkingDirectory = riderPath,
+				                         WorkingDirectory = _cachedRiderPath,
 				                         FileName = "rider",
 				                         Arguments = $"--line {stackFrame.Line} --column {stackFrame.Column + 4} {stackFrame.FileFullPath}",
 				                         CreateNoWindow = true,
