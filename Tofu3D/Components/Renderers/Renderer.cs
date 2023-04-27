f58906a02f1acc75e218d70c77e881404fd3bee4
hexiy <16806036+hexiy@users.noreply.github.com>
2023-04-27 23:11:29 +0200

    spriterenderer pivot

--- Tofu3D/Components/Renderers/Renderer.cs
+++ Tofu3D/Components/Renderers/Renderer.cs
@@ -109,12 +109,10 @@ public class Renderer : Component, IComparable<Renderer>
 
 	public Matrix4x4 GetModelMatrixForCanvasObject()
 	{
-		Vector3 pivotOffset = -(BoxShape.Size * Transform.WorldScale) / 2
-		                    + new Vector3(BoxShape.Size.X * Transform.WorldScale.X * Transform.Pivot.X,
-		                                  BoxShape.Size.Y * Transform.WorldScale.Y * Transform.Pivot.Y,
-		                                  BoxShape.Size.Z * Transform.WorldScale.Z * Transform.Pivot.Z);
+		Vector3 worldPositionPivotOffset = BoxShape.Size * Transform.WorldScale * (Vector3.One - Transform.Pivot * 2);
 
-		Matrix4x4 pivot = Matrix4x4.CreateTranslation(-pivotOffset.X, -pivotOffset.Y, -pivotOffset.Z);
+		Matrix4x4 pivot = Matrix4x4.CreateTranslation(worldPositionPivotOffset);
+
 		Matrix4x4 translation = Matrix4x4.CreateTranslation(Transform.WorldPosition - (Camera.MainCamera.Size / 2) + BoxShape.Offset * Transform.WorldScale + (GameObject.IndexInHierarchy * Vector3.One * 0.0001f)) * Matrix4x4.CreateScale(1, 1, 1);
 
 		Matrix4x4 rotation = Matrix4x4.CreateFromYawPitchRoll(Transform.WorldRotation.Y / 180 * Mathf.Pi,
