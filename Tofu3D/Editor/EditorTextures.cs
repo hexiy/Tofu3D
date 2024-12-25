df59b4b942910dd153d4cd417c08667b4ca4a66a
hexiy <slavo.banas@icloud.com>
2024-12-25 19:38:05 +0100

    editortextures load in constructor

--- Tofu3D/Editor/EditorTextures.cs
+++ Tofu3D/Editor/EditorTextures.cs
@@ -2,9 +2,18 @@ namespace Tofu3D;
 
 public class EditorTextures
 {
-    public RuntimeTexture LogCategoryErrorIcon = Tofu.AssetLoadManager.Load<RuntimeTexture>("Resources/Console/error.png");
-    public RuntimeTexture LogCategoryInfoIcon = Tofu.AssetLoadManager.Load<RuntimeTexture>("Resources/Console/info.png");
-    public RuntimeTexture LogCategoryTimerIcon = Tofu.AssetLoadManager.Load<RuntimeTexture>("Resources/Console/timer.png");
-    public RuntimeTexture LogCategoryWarningIcon = Tofu.AssetLoadManager.Load<RuntimeTexture>("Resources/Console/warning.png");
-    public RuntimeTexture WhitePixel = Tofu.AssetLoadManager.Load<RuntimeTexture>("Resources/whitePixel.png");
+    public readonly RuntimeTexture LogCategoryErrorIcon;
+    public readonly RuntimeTexture LogCategoryInfoIcon;
+    public readonly RuntimeTexture LogCategoryTimerIcon;
+    public readonly RuntimeTexture LogCategoryWarningIcon;
+    public readonly RuntimeTexture WhitePixel;
+
+    public EditorTextures()
+    {
+        LogCategoryErrorIcon = Tofu.AssetLoadManager.Load<RuntimeTexture>("Resources/Console/error.png");
+        LogCategoryInfoIcon = Tofu.AssetLoadManager.Load<RuntimeTexture>("Resources/Console/info.png");
+        LogCategoryTimerIcon = Tofu.AssetLoadManager.Load<RuntimeTexture>("Resources/Console/timer.png");
+        LogCategoryWarningIcon = Tofu.AssetLoadManager.Load<RuntimeTexture>("Resources/Console/warning.png");
+        WhitePixel = Tofu.AssetLoadManager.Load<RuntimeTexture>("Resources/whitePixel.png");
+    }
 }
