64bcc049de3209b1abbff18b11894409f8f62c78
hexiy <slavo.banas@icloud.com>
2024-12-30 13:42:23 +0100

    lower res scene thumbnails

--- Tofu3D/FramebufferScreenshotGenerator.cs
+++ Tofu3D/FramebufferScreenshotGenerator.cs
@@ -17,23 +17,29 @@ public static class FramebufferScreenshotGenerator
         fileName = fileName.Replace(".tofutexture", "");
 
         framebuffer.Bind();
-        byte[] framebufferData = new byte[(int)framebuffer.Size.X * (int)framebuffer.Size.Y * 4];
+        byte[] framebufferData = new byte[framebuffer.Size.Xi * framebuffer.Size.Yi * 4];
         GL.ReadPixels(0, 0, framebuffer.Size.Xi, framebuffer.Size.Yi, PixelFormat.Rgba, PixelType.UnsignedByte,
             ref framebufferData[0]);
-        using (var image = Image.LoadPixelData<Rgba32>(framebufferData, framebuffer.Size.Xi, framebuffer.Size.Yi))
+        
+        using var image = Image.LoadPixelData<Rgba32>(framebufferData, framebuffer.Size.Xi, framebuffer.Size.Yi);
+        Vector2 newSize = framebuffer.Size / 5f;
+
+        image.Mutate(x =>
         {
-            image.Mutate(x => x.Flip(FlipMode.Vertical));
-            byte[] pixels = new byte[framebufferData.Length];
-            image.CopyPixelDataTo(pixels);
+            x.Flip(FlipMode.Vertical);
+            x.Resize(newSize.Xi, newSize.Yi);
+        });
+        byte[] pixels = new byte[newSize.Xi*newSize.Yi * 4];
+        image.CopyPixelDataTo(pixels);
 
-            Asset_Texture texture = new Asset_Texture() { Pixels = pixels, TextureSize = framebuffer.Size };
+        Asset_Texture texture = new Asset_Texture() { Pixels = pixels, TextureSize = newSize };
 
-            string path = Path.Combine(Folders.SceneThumbnailsInLibrary,
-                fileName);
-            string tofuTexturePath = path + ".tofutexture";
-            Serializer.SaveAssetJSON<Asset_Texture>(tofuTexturePath, texture);
-            image.SaveAsPng(path);
-        }
+        string path = Path.Combine(Folders.SceneThumbnailsInLibrary,
+            fileName);
+        string tofuTexturePath = path + ".tofutexture";
+        Serializer.SaveAssetJSON<Asset_Texture>(tofuTexturePath, texture);
+        image.SaveAsPng(path);
+
 
         framebuffer.Unbind();
     }
