8eddb2caca9b4b91964c97b1da33872b3118f111
hexiy <slavo.banas@icloud.com>
2025-02-02 22:35:11 +0100

    string as path in inspector

--- Tofu3D/Editor/InspectorFieldDrawerString.cs
+++ Tofu3D/Editor/InspectorFieldDrawerString.cs
@@ -9,36 +9,39 @@ public class InspectorFieldDrawerString : InspectorFieldDrawable<string>
     {
         string? fieldValue = GetValue(info, componentInspectorData);
 
-        bool changed = false;
-
-
         info.GetCustomAttribute<PathString>(out PathString? pathStringAttrib);
         if (pathStringAttrib != null)
         {
-            bool clicked = ImGui.Button("$");
-            ImGui.SameLine();
+            string buttonLabel = fieldValue.Length > 0 ? fieldValue : "<path>";
+            bool clicked = ImGui.Button(buttonLabel, new Vector2(ImGui.GetContentRegionAvail().X, 30));
+            
+            // ImGui.SameLine();
             if (clicked)
             {
                 DialogResult dialogResult = Dialog.FolderPicker(null);
                 if (dialogResult.IsOk)
                 {
                     fieldValue = dialogResult.Path;
-                    changed = true;
+
+                    SetValue(info, componentInspectorData, fieldValue);
                 }
             }
+
+            if (ImGui.IsItemClicked(ImGuiMouseButton.Right))
+            {
+                SetValue(info, componentInspectorData, "");
+            }
+
+            return;
         }
 
         float widthAvailable = ImGui.GetContentRegionAvail().X;
-        Vector2 textSize = ImGui.CalcTextSize($"{fieldValue} ", wrapWidth: widthAvailable); // add 1 space so new line changes the height
+        Vector2 textSize =
+            ImGui.CalcTextSize($"{fieldValue} ",
+                wrapWidth: widthAvailable); // add 1 space so new line changes the height
         textSize.Y = Mathf.ClampMin(textSize.Y, 35);
         if (ImGui.InputTextMultiline("", ref fieldValue, 10_000,
                 new System.Numerics.Vector2(widthAvailable, textSize.Y + 10)))
-        {
-            changed = true;
-        }
-
-
-        if (changed)
         {
             SetValue(info, componentInspectorData, fieldValue);
         }
