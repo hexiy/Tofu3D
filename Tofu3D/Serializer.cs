40e232622de7b03d5425b3d9952599e61ca0765f
hexiy <slavo.banas@icloud.com>
2025-02-11 15:14:01 +0100

    serialization tweaks

--- Tofu3D/Serializer.cs
+++ Tofu3D/Serializer.cs
@@ -4,6 +4,8 @@ using Newtonsoft.Json;
 
 public static class Serializer
 {
+    private static JsonSerializer _jsonSerializer = new JsonSerializer() { Formatting = Formatting.Indented };
+
     public static void SaveAssetJSON<T>(string path, AssetBase asset)
     {
         asset.BeforeSerialized();
@@ -25,14 +27,22 @@ public static class Serializer
 
     public static void SaveFileJSON<T>(string path, object content)
     {
+        // using (FileStream stream = new FileStream(path, FileMode.OpenOrCreate))
+        // {
+        //     using (BinaryWriter writer = new BinaryWriter(stream, Encoding.UTF8, false))
+        //     {
+        //         writer.Write(JsonConvert.SerializeObject(content, Formatting.Indented));
+        //     }
+        // }
+
         using (FileStream stream = new FileStream(path, FileMode.OpenOrCreate))
+        using (StreamWriter streamWriter = new StreamWriter(stream, Encoding.UTF8))
+        using (JsonTextWriter jsonWriter = new JsonTextWriter(streamWriter))
         {
-            using (BinaryWriter writer = new BinaryWriter(stream, Encoding.UTF8, false))
-            {
-                writer.Write(JsonConvert.SerializeObject(content, Formatting.Indented));
-            }
+            _jsonSerializer.Serialize(jsonWriter, content);
         }
     }
+
     public static void SaveTextFile(string path, string text)
     {
         using (FileStream stream = new FileStream(path, FileMode.OpenOrCreate))
@@ -48,9 +58,19 @@ public static class Serializer
     {
         if (File.Exists(path) == false)
         {
-            Debug.LogError("ReadFileJSON failed, file doesn't exist");
+            Debug.LogError($"ReadFileJSON failed, file doesn't exist: {path}");
             return default;
         }
+
+        // jsonReader streams the data so it doesnt have to allocate huge amounts of data for deserializing big files
+        using (FileStream stream = new FileStream(path, FileMode.Open))
+        using (StreamReader streamReader = new StreamReader(stream, Encoding.UTF8))
+        using (JsonTextReader jsonReader = new JsonTextReader(streamReader))
+        {
+            // JsonSerializer serializer = new JsonSerializer();
+            return _jsonSerializer.Deserialize<T>(jsonReader);
+        }
+
 
         using (FileStream stream = new FileStream(path, FileMode.Open))
         {
