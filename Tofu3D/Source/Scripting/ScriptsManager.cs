dd73f6f3ecc735fa9dfddc106c765b5f19f1afde
hexiy <slavo.banas@icloud.com>
2025-05-19 13:44:35 +0200

    scripts from project folder now load

--- Tofu3D/Source/Scripting/ScriptsManager.cs
+++ Tofu3D/Source/Scripting/ScriptsManager.cs
@@ -35,6 +35,56 @@ public static class ScriptsManager
 
     public static void CompileScriptsAssembly()
     {
-        ScriptsAssembly = typeof(Scripts.Component).Assembly;
+        if (_scriptLoadContext != null)
+        {
+            _scriptLoadContext.Unload();
+            _scriptLoadContext = null;
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+        }
+
+        string[] scriptsFiles = Directory.GetFiles(Folders.Scripts, "*.cs");
+
+        IEnumerable<SyntaxTree> syntaxTrees = scriptsFiles
+            .Select(file => CSharpSyntaxTree.ParseText(File.ReadAllText(file)));
+
+        string assemblyName = "Scripts.dll";
+
+        string[] referencesPaths = new[]
+        {
+            typeof(object).Assembly.Location,
+            // Assembly.Load("netstandard").Location,
+            Assembly.Load("System.Runtime").Location,
+            Assembly.Load("Tofu3D").Location,
+        };
+        PortableExecutableReference[] references = new PortableExecutableReference[referencesPaths.Length];
+        for (int i = 0; i < referencesPaths.Length; i++)
+        {
+            references[i] = MetadataReference.CreateFromFile(referencesPaths[i]);
+        }
+
+        CSharpCompilation compilation = CSharpCompilation.Create(assemblyName)
+            .AddSyntaxTrees(syntaxTrees)
+            .WithReferences(references)
+            .WithOptions(new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+
+        using MemoryStream ms = new();
+        EmitResult emitResult = compilation.Emit(ms);
+
+        if (!emitResult.Success)
+        {
+            string errors = string.Join("\n", emitResult.Diagnostics
+                .Where(d => d.Severity == DiagnosticSeverity.Error)
+                .Select(d => d.GetMessage()));
+            throw new Exception($"Scripts.dll compilation failed:\n{errors}");
+        }
+
+        ms.Seek(0, SeekOrigin.Begin);
+
+
+        _scriptLoadContext = new ScriptLoadContext();
+        ScriptsAssembly = Assembly.Load(ms.ToArray());
+        // ScriptsAssembly = _scriptLoadContext.LoadFromStream(ms);
     }
 }
