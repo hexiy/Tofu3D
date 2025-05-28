d614008e62c9c12e01422539f74d64b4d7e4859b
hexiy <slavo.banas@icloud.com>
2025-05-28 20:24:50 +0200

    fix imgui window padding changed when game view is not visible

--- Tofu3D/Source/Editor/EditorPanel.cs
+++ Tofu3D/Source/Editor/EditorPanel.cs
@@ -79,6 +79,7 @@ public abstract class EditorPanel
             if (IsVisible == false)
             {
                 ImGui.End();
+                AfterWindowEnded();
                 return;
             }
 
